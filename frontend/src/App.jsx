1de02b96df6336220e569f8e5e9185d2cc9f135c
m20arcusk <marcuskam20@gmail.com>
2025-02-09 17:36:25 -0800

    griddyboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Griddy from "./pages/Griddy";
 import HomePage from './components/appHome/home'
 import MainPage from './components/appHome/main'
+import GriddyBoard from "./components/appHome/griddyBoard";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/griddy" element={<Griddy />} />
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/griddyboard" element={<GriddyBoard />} />
       </Routes>
     </div>
   );
