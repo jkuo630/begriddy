68a92d7679832d33c6409d2da5b9fff8c800d756
jasonkuoubc <jason.kuo@ubc.ca>
2025-02-09 17:52:28 -0800

    routing fix

--- frontend/src/components/griddy/navbar.jsx
+++ frontend/src/components/griddy/navbar.jsx
@@ -1,10 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function Header() {
+  const navigate = useNavigate(); // Initialize the navigate function
+
   return (
     <header className="fixed top-0 left-0 w-full shadow-md bg-gray-900/50 backdrop-blur-md z-50 mb-10 flex justify-center items-center h-16">
       <KeyboardArrowDownIcon className="absolute left-4 text-white" />
-      <h1 className="text-2xl font-bold text-white">BeGriddy.</h1>
+
+      {/* Make BeGriddy title clickable */}
+      <h1
+        className="text-2xl font-bold text-white cursor-pointer"
+        onClick={() => navigate("/main")} // Navigate to /main when clicked
+      >
+        BeGriddy.
+      </h1>
     </header>
   );
 }
--- frontend/src/components/results/navbar.jsx
+++ frontend/src/components/results/navbar.jsx
@@ -1,8 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import IosShareIcon from "@mui/icons-material/IosShare";
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <header className="fixed top-0 left-0 w-full shadow-md bg-gray-900/50 backdrop-blur-md z-50 flex justify-between items-center px-4 h-16">
       <KeyboardArrowDownIcon
@@ -10,9 +14,13 @@ function Navbar() {
         fontSize="large"
       />
 
-      <h1 className="text-2xl font-bold text-white">BeGriddy.</h1>
+      <h1
+        className="text-2xl font-bold text-white cursor-pointer"
+        onClick={() => navigate("/main")}
+      >
+        BeGriddy.
+      </h1>
 
-      {/* Share and More Horiz icons */}
       <div className="flex space-x-4">
         <IosShareIcon
           className="text-white cursor-pointer"
